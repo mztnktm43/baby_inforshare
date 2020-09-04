2bc593992430f07c3f481865d182faa68934efd6
mztnktm43 <spngkob.714bd@gmail.com>
2020-09-04 19:15:18 +0900

    items#indexアクションの設定01回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  # extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  has_noe_attahed :image
+  # has_noe_attahed :image
   has_many :comments
   belongs_to_active_hash :category
   belongs_to_active_hash :age_year
