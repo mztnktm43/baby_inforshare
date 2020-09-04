b9a51c86e4e4068039c5d33599d47b8c10e30383
mztnktm43 <spngkob.714bd@gmail.com>
2020-09-04 20:28:26 +0900

    new/createアクションの設定01回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,19 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order('created_at DESC')
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def creat
+    Item.create(item_params)
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :age_yasr_id, :age_month_id, :size, :price).merge(user_id: current_user_id)
+  end
 end
