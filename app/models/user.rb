402d2dab5cced67d7effffc354a392ab767c0fe3
mztnktm43 <spngkob.714bd@gmail.com>
2020-09-07 20:21:36 +0900

    ログイン/ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :comments
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze # 半角英数混合
+
+  validates :nickname, presence: true
+  validates :password, presence: true, format: { with: /\d{6}/, with: PASSWORD_REGEX } # ６文字以上
+
 end
