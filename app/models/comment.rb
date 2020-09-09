472b40aac259e8a51306bae8a70059e92f0dddda
mztnktm43 <spngkob.714bd@gmail.com>
2020-09-09 23:32:52 +0900

    Commentモデルの単体テストコード

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :item
 
-  validates :comment, presence: true
+  validates :comment, presence: true, length: { maximum: 1000 }
 
 end
