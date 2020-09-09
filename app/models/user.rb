9a36af8d66e504e30750f8978112d0fbd2327b07
mztnktm43 <spngkob.714bd@gmail.com>
2020-09-09 18:56:19 +0900

    テストコードの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze # 半角英数混合
 
-  validates :nickname, presence: true, uniqueness: true, length: { maximum: 15 }
+  validates :nickname, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 15 }
   validates :email, presence: true
   validates :password, presence: true, format: { with: /\d{6}/, with: PASSWORD_REGEX } # ６文字以上
 end
