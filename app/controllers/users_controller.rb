4818bcf1387b33bb9096a0bd641961435cd3f6f3
mztnktm43 <spngkob.714bd@gmail.com>
2020-09-11 12:37:38 +0900

    userマイページにedit/destroyボタン表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @items = user.items.order('created_at DESC')
+    @user = User.find(params[:id])
+    @items = @user.items.order('created_at DESC')
   end
 end
