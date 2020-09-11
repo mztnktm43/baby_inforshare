c9abbc231134ab5ccd59d2d26130054d2c789905
mztnktm43 <spngkob.714bd@gmail.com>
2020-09-11 18:56:41 +0900

    edit/destroy機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,28 @@ class UsersController < ApplicationController
     @nickname = user.nickname
     @items = user.items.order('created_at DESC')
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to new_user_session_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :parent_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
+<<<<<<< Updated upstream
   resources :items
   resources :users, only: :show
+=======
+  resources :items do
+    resources :comments, only: :create
+  end
+  resources :users, only: [:show, :edit, :update, :destroy]
+>>>>>>> Stashed changes
 end
