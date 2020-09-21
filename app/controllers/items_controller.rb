d3bcc98fcf9dc3a35042e3bb58c385903375ae65
mztnktm43 <spngkob.714bd@gmail.com>
2020-09-21 22:19:29 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,6 +54,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_new
-    redirect_to action: :index unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
